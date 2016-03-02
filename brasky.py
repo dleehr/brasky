d403e876da48524ec00fa9c15a6f97b7c1202d0c
Dan Leehr <dan.leehr@duke.edu>
2016-03-02 11:50:20 -0500

    Brasky as a service

---
+++ brasky.py
@@ -0,0 +1,17 @@
+from flask import Flask
+app = Flask(__name__)
+import random
+
+def load_quotes():
+    with open('quotes.txt', 'r') as f:
+        quotes = [line.decode('utf-8').strip() for line in f.readlines()]
+    return quotes
+
+@app.route("/")
+def brasky():
+    quotes = load_quotes()
+    index = random.randrange(0,len(quotes))
+    return quotes[index]
+
+if __name__ == "__main__":
+    app.run()
